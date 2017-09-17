f63017dff73d8863130446c82a0c86ea51a5d6ec
zhvirblis <yurocker@mail.ru>
2017-09-17 21:21:04 +0300

    some implements

--- task/01-strings-tasks.js
+++ task/01-strings-tasks.js
@@ -22,7 +22,7 @@
  *   '',  'bb'  => 'bb'
  */
 function concatenateStrings(value1, value2) {
-    throw new Error('Not implemented');
+    return value1+value2;
 }
 
 
@@ -38,7 +38,7 @@ function concatenateStrings(value1, value2) {
  *   ''      => 0
  */
 function getStringLength(value) {
-    throw new Error('Not implemented');
+    return value.length;
 }
 
 /**
@@ -55,7 +55,7 @@ function getStringLength(value) {
  *   'Chuck','Norris'  => 'Hello, Chuck Norris!'
  */
 function getStringFromTemplate(firstName, lastName) {
-    throw new Error('Not implemented');
+    return 'Hello, ' + firstName + ' ' + lastName+ '!';
 }
 
 /**
@@ -69,7 +69,7 @@ function getStringFromTemplate(firstName, lastName) {
  *   'Hello, Chuck Norris!' => 'Chuck Norris'
  */
 function extractNameFromTemplate(value) {
-    throw new Error('Not implemented');
+    return value.substr(7,value.length-8);
 }
 
 
@@ -84,7 +84,7 @@ function extractNameFromTemplate(value) {
  *   'cat'       => 'c'
  */
 function getFirstChar(value) {
-    throw new Error('Not implemented');
+    return value[0];
 }
 
 /**
@@ -99,7 +99,7 @@ function getFirstChar(value) {
  *   '\tHello, World! ' => 'Hello, World!'
  */
 function removeLeadingAndTrailingWhitespaces(value) {
-    throw new Error('Not implemented');
+    return value.trim();
 }
 
 /**
@@ -114,7 +114,7 @@ function removeLeadingAndTrailingWhitespaces(value) {
  *   'cat', 3 => 'catcatcat'
  */
 function repeatString(value, count) {
-    throw new Error('Not implemented');
+    return value.repeat(count);
 }
 
 /**
@@ -130,7 +130,7 @@ function repeatString(value, count) {
  *   'ABABAB','BA' => 'ABAB'
  */
 function removeFirstOccurrences(str, value) {
-    throw new Error('Not implemented');
+    return str.replace(value,'');
 }
 
 /**
@@ -145,7 +145,7 @@ function removeFirstOccurrences(str, value) {
  *   '<a>' => 'a'
  */
 function unbracketTag(str) {
-    throw new Error('Not implemented');
+    return(str.slice(1,str.length-1));
 }
 
 
@@ -160,7 +160,7 @@ function unbracketTag(str) {
  *  'abcdefghijklmnopqrstuvwxyz' => 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
  */
 function convertToUpperCase(str) {
-    throw new Error('Not implemented');
+    return str.toUpperCase();
 }
 
 /**
@@ -174,7 +174,7 @@ function convertToUpperCase(str) {
  *   'info@gmail.com' => ['info@gmail.com']
  */
 function extractEmails(str) {
-    throw new Error('Not implemented');
+    return str.split(';');
 }
 
 /**
@@ -201,7 +201,21 @@ function extractEmails(str) {
  *
  */
 function getRectangleString(width, height) {
-    throw new Error('Not implemented');
+    let str='';
+    for(var i=0;i < height; i++){
+        for (var j = 0; j < width; j++) {
+            if(i==0 && j==0) str+='┌';
+            else if(i==0 && j!=width-1) str+='─';
+            else if(j==width-1 && i==height-1) str+='┘\n';
+            else if(i==0 && j==width-1) str+='┐\n';
+            else if(j!=0 && i==height-1) str+='─';
+            else if(j==0 && i==height-1) str+='└';
+            else if(i!=0 && j==0) str+='│';
+            else if(i!=0 && j==width-1) str+='│\n';
+            else str+=' ';
+        }
+    }
+    return str;
 }
 
 
@@ -238,7 +252,10 @@ function encodeToRot13(str) {
  *   isString(new String('test')) => true
  */
 function isString(value) {
-    throw new Error('Not implemented');
+    if(typeof value=='object'){
+        return Object.prototype.toString.call(value)=='[object String]';
+    }
+    return (typeof value)=='string';
 }
 
 
@@ -267,7 +284,23 @@ function isString(value) {
  *   'K♠' => 51
  */
 function getCardId(value) {
-    throw new Error('Not implemented');
+    let code = {A:0, J:10, Q:11, K:12};
+    let symbols = {'♣':0,'♦':1,'♥':2, '♠':3};
+    let index = 0;
+
+    if(code[value[0]] || code[value[0]]===0){
+        index+=code[value[0]];
+    }
+    else{
+        index+=Number(value[0])-1;
+    }
+    if(value[1]=='0'){
+        index=symbols[value[1]]*13+10;
+    }
+    else{
+        index+=symbols[value[1]]*13;
+    }
+    return index;
 }
 
 
