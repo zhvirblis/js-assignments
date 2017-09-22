df9bce06ef36fd3f2fa38eda0d52faa708268e57
Vadim <vadim54787@gmail.com>
2017-09-22 09:52:35 +0300

    additional test for domino task

--- test/10-katas-1-tests.js
+++ test/10-katas-1-tests.js
@@ -162,6 +162,8 @@ describe('10-katas-1-tasks', function() {
                  [1,3], [2,3], [1,4], [2,4], [1,5], [2,5]
              ],[
                  [1,1], [1,2], [2,3], [2,5], [2,6], [3,6], [5,6], [6,6]
+             ],[
+                 [1,2], [2,1], [2,2]
              ]
          ].forEach(data => {
             var actual = tasks.canDominoesMakeRow(data);
