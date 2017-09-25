a63bfb583b31f9e9a86fe893eddd3626e298726b
zhvirblis <zhvirblis@github.com>
2017-09-25 16:46:01 +0300

    Not full

--- task/08-objects-tasks.js
+++ task/08-objects-tasks.js
@@ -23,7 +23,11 @@
  *    console.log(r.getArea());   // => 200
  */
 function Rectangle(width, height) {
-    throw new Error('Not implemented');
+    this.width = width;
+    this.height = height;
+    this.getArea = function(){
+        return width * height;        
+    }
 }
 
 
@@ -38,7 +42,7 @@ function Rectangle(width, height) {
  *    { width: 10, height : 20 } => '{"height":10,"width":20}'
  */
 function getJSON(obj) {
-    throw new Error('Not implemented');
+    return JSON.stringify(obj);
 }
 
 
